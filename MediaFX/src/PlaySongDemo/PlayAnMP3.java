c4574de7ddf3f63ae0de08ba7e5cd1ddc75503a1
nash981 <nishant3j@arizona.edu>
2023-03-01 20:48:05 -0700

    first commit

--- MediaFX/src/PlaySongDemo/PlayAnMP3.java
+++ MediaFX/src/PlaySongDemo/PlayAnMP3.java
@@ -1,3 +1,4 @@
+
 package PlaySongDemo;
 
 /**
@@ -28,7 +29,8 @@ public class PlayAnMP3 extends Application {
   @Override
   public void start(Stage stage) throws Exception {
     BorderPane pane = new BorderPane();
-    String path = "songfiles/Capture.mp3";
+    // Nishant changed the song
+    String path = "songfiles/DanseMacabreViolinHook.mp3";
     pane.setCenter( new Label(path));
     playASong(path);
     // Put the pane in a sized Scene and show the GUI
---
+++ MediaFX/src/module-info.java
@@ -0,0 +1,6 @@
+module MediaFX {
+	exports PlaySongDemo;
+	requires transitive javafx.graphics;
+	requires javafx.media;
+	requires javafx.controls;
+}
